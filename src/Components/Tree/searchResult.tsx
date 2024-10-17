2f52749c478c59f990b6c7b0f73af78b7163cea5
s.navidi <shnavidi18@gmail.com>
2024-10-17 17:49:03 +0330

    Add orgchart icon

--- src/Components/Tree/searchResult.tsx
+++ src/Components/Tree/searchResult.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NodeType } from '../../types';
 import ArrowUpIcon from '../SvgIcons/arrow-up';
 import ArrowDownIcon from '../SvgIcons/arrow-down';
+import OrgchartIcon from '../SvgIcons/orgchart';
 interface Props {
   items: (NodeType & { hierarchy: string[] })[];
   searchResultVisible: boolean;
@@ -25,17 +26,25 @@ function SearchResult({
     >
       {searchResultVisible ? (
         <div className="iconContainer">
-          <div className="arrowIcon">
+          <div className="iconClass">
             <ArrowUpIcon />
           </div>
-          {
-            items.length > 0  ? items.map((item) => (
-              <div className='searchResultItem'>{item.title}</div>
-            )) : <h5>نتیجه‌ای برای جستجوی شما وجود نداشت.</h5>
-          }
-          </div> ) : (
+          {items.length > 0 ? (
+            items.map((item) => (
+              <div className="searchResultItem">
+                <span>{item.title}</span>
+                <div className="iconClass">
+                  <OrgchartIcon />
+                </div>
+              </div>
+            ))
+          ) : (
+            <h5>نتیجه‌ای برای جستجوی شما وجود نداشت.</h5>
+          )}
+        </div>
+      ) : (
         <div className="iconContainer">
-          <div className="arrowIcon">
+          <div className="iconClass">
             <ArrowDownIcon />
           </div>
         </div>
